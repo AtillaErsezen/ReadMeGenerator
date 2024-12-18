fca072ddba1e3bf1edccf23cea627e69cca4f520
Atilla Ersezen <ersezenatilla@gmail.com>
2024-12-18 14:13:48 +0100

    Update App.js
    Changed allow browser surfing for safety

--- read_me_generator/src/App.js
+++ read_me_generator/src/App.js
@@ -4,7 +4,7 @@ import OpenAI from "openai";
 
 const openai = new OpenAI({
     apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true,
+    dangerouslyAllowBrowser: false,
   });
 
 function App() {
